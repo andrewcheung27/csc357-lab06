bb691d69a026f65b6c6facd2bb97d18a51acbc4b
andrewcheung27 <andrewcheung27@gmail.com>
2022-05-18 11:06:26 -0700

    working, combine to one itimer and block signals

--- timeit.c
+++ timeit.c
@@ -3,11 +3,51 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/time.h>
+#include <unistd.h>
+
+
+static int setTimers(struct itimerval *tick, struct itimerval *tock) {
+    /* tick every second, starting immediately */
+    tick->it_value.tv_sec = 0;
+    tick->it_value.tv_usec = 5000000;
+    tick->it_interval.tv_sec = 1;
+    tick->it_interval.tv_usec = 0;
+
+    /* tock every second, starting at a half-second offset */
+    tock->it_value.tv_sec = 0;
+    tock->it_value.tv_usec = 1;
+    tock->it_interval.tv_sec = 1;
+    tock->it_interval.tv_usec = 0;
+
+    setitimer(ITIMER_REAL, tick, 0);
+    setitimer(ITIMER_REAL, tock, 0);
+
+    return 0;
+}
+
+
+void handler(int signum, siginfo_t *siginfo, void *ucontext) {
+    static int n = 1;
+
+    if (n++ % 2) {
+        printf("Tick...");
+    }
+    else {
+        printf("Tock\n");
+    }
+
+    fflush(stdout);
+}
 
 
 int main(int argc, char *argv[]) {
     long seconds;
     char *c;
+
+    struct itimerval tickTimer;
+    struct itimerval tockTimer;
+    struct sigaction sa;
+
 
     /* not enough args */
     if (argc < 2) {
@@ -30,10 +70,21 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    /* timer stuff */
-    printf("seconds = %ld\n", seconds);
-    fflush(stdout);
 
+    /* register handler for SIGALRM */
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_sigaction = handler;
+    sigaction(SIGALRM, &sa, 0);
+
+    setTimers(&tickTimer, &tockTimer);
+
+    /* possible overflow? */
+    seconds *= 2;
+    while (seconds--) {
+        pause();
+    }
+
+    printf("Time's up!\n");
     return 0;
 }
 
